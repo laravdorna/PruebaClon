23b979aea44c5f7aec0490551d8569af1d29eb1c
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2018-10-11 09:46:18 +0200

    prueba subida para poder clonar

---
+++ src/pruebaclonargit/PruebaClonarGit.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaclonargit;
+
+/**
+ * Esto es una prueba de clonar el proyecto en el git
+ *
+ * @author lvazquezdorna
+ */
+public class PruebaClonarGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Prueba de clonar git");
+        int prueba;
+        float p;
+        
+
+    }
+
+}
